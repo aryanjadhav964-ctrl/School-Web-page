20d1894dc53f764970cd53b6baf18edc6583ea17
aryanjadhav <aryanjadhav964@gmail.com>
2025-12-28 13:38:05 +0000

    navbar update

---
+++ frontend/js/navToggleMenu.js
@@ -0,0 +1,8 @@
+// Toggle menu on small screens
+      const hamburger = document.querySelector(".hamburger");
+      const navLinks = document.querySelector(".nav-links");
+
+      hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+        hamburger.classList.toggle("toggle");
+      });
